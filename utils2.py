84541b232352a58b84c1298736f24a34c00ede5c
gooh123 <101920016+gooh123@users.noreply.github.com>
2022-04-01 00:38:05 +0300

    Update utils2.py

--- utils2.py
+++ utils2.py
@@ -1,26 +1,28 @@
 from flask import render_template, Blueprint, request
 import logging
-from utils import get_posts_all, load_json_from_file, search_for_posts
+import utils
 from exceptions import DataLayerError
 
 main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates')
 logging.basicConfig(filename='basic.log', level=logging.INFO)
+
 DATA_FILE_PATH = 'static/data/data.json'
+COMMENTS_FILE_PATH = 'static/data/comments.json'
 
 
 @main_blueprint.route('/')
 def main_page():
-    data = load_json_from_file(DATA_FILE_PATH)
+    data = utils.load_json_from_file(DATA_FILE_PATH)
     logging.info("запрошена главная страничка")
     return render_template('index.html', information="content", posts=data)
 
 
-@main_blueprint.route('/search')
+@main_blueprint.route('/search', methods=['GET'])
 def search_page():
     s = request.args.get('s', "")
     logging.info("Выполняется поиск")
     try:
-        posts = search_for_posts(s)
+        posts = utils.search_for_posts(s)
         return render_template('search.html', posts=posts, s=s)
     except DataLayerError:
         return "поврежден файл с данными"
@@ -28,20 +30,24 @@ def search_page():
 
 @main_blueprint.route('/post/<int:pk>')
 def post_info(pk):
-    data = load_json_from_file(DATA_FILE_PATH)
+    post = utils.get_post_by_pk(pk)
+    comments = utils.get_comments_by_post_id(pk)
+
     logging.info("запрошена страничка с постом")
-    return render_template('post.html', post='pk')
+
+    return render_template('post.html', post=post, comments=comments)
 
 
-@main_blueprint.route('/user-feed/<int:poster_name>')
+@main_blueprint.route('/user-feed/<string:poster_name>')
 def user_feed(poster_name):
-    data = load_json_from_file(DATA_FILE_PATH)
+    data = utils.get_posts_by_user(poster_name)
+
     logging.info("запрошена страничка с пользователем")
-    return render_template('user-feed.html', post='poster_name')
+
+    return render_template('user-feed.html', post='poster_name', posts=data)
 
 
-@main_blueprint.route('/bookmarks/<int:id>')
-def bookmarks():
-    data = load_json_from_file(DATA_FILE_PATH)
-    logging.info("запрошена страничка с bookmarks")
-    return render_template('bookmarks.html')
+@main_blueprint.route('/bookmarks/<string:mark>')
+def bookmarks(mark):
+    return render_template('bookmarks.html', post=mark)
+
