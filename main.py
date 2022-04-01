b374bf45430cc4e386d3ac60dd0474ea86652893
gooh123 <101920016+gooh123@users.noreply.github.com>
2022-04-01 11:12:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, Blueprint, request
 from utils2 import main_blueprint
-
+from views import views_blueprint
 import logging
 
 logging.basicConfig(filename='basic.log', level=logging.INFO)
@@ -10,5 +10,6 @@ UPLOAD_FOLDER = 'static/img'
 
 app = Flask(__name__)
 app.register_blueprint(main_blueprint)
+app.register_blueprint(views_blueprint)
 
 app.run(debug=True)
